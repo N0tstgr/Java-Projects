73e82cc01b50ecd0844908cbab2de65dc46763b4
Your Name <vikashkasaudhan321@gmail.com>
2025-09-21 19:30:09 +0530

    gen2

---
+++ Stranger/src/Stgr/DigitalClock.java
@@ -0,0 +1,12 @@
+package Stgr;
+import javax.swing.*;
+public class ChattingApplication extends JFrame {
+
+    Server class Server{
+
+    }
+    public static void main(String[] args) {
+        new Server();
+
+    }
+}
