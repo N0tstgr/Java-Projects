6b0a73346bb635bae91835458d6e9f2f1d224ac6
Your Name <vikashkasaudhan321@gmail.com>
2025-09-22 23:51:00 +0530

    Imageicon

--- Stranger/src/BankManagementSystem/Login.java
+++ Stranger/src/BankManagementSystem/Login.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class Login extends JFrame {
     Login(){
         setTitle("AUTOMATED TELLER MACHINE(ATM)");
+        ImageIcon i1   = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        JLabel label = new JLabel(i1);
+        add(label);
         setSize(810, 490);
         setVisible(true);
         setLocation(400, 150);
