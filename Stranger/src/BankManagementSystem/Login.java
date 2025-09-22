87472ed2c45d5ae47b5dfc3e08c9e8d914336a92
Your Name <vikashkasaudhan321@gmail.com>
2025-09-23 00:09:15 +0530

    setBounds

--- Stranger/src/BankManagementSystem/Login.java
+++ Stranger/src/BankManagementSystem/Login.java
@@ -6,12 +6,14 @@ import java.awt.*;
 public class Login extends JFrame {
     Login(){
         setTitle("AUTOMATED TELLER MACHINE(ATM)");
+        setLayout(null);
         ImageIcon i1   = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
 
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
 
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);
+        label.setBounds(70,10,100,100);
         add(label);
         setSize(810, 490);
         setVisible(true);
