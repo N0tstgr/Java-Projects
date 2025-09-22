4583af7ef842d0c9bb25eda2ae3622836b9c8a8b
Your Name <vikashkasaudhan321@gmail.com>
2025-09-22 23:37:45 +0530

    SEtting title

--- Stranger/src/BankManagementSystem/Login.java
+++ Stranger/src/BankManagementSystem/Login.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 public class Login extends JFrame {
     Login(){
+        setTitle("AUTOMATED TELLER MACHINE(ATM)");
         setSize(810, 490);
         setVisible(true);
         setLocation(400, 150);
