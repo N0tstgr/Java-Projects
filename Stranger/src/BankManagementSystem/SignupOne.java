4b1ab7ba5a2568a25c4cc0ea0af001423016176b
Your Name <vikashkasaudhan321@gmail.com>
2025-09-24 01:08:25 +0530

    rand+apply

--- Stranger/src/BankManagementSystem/SignupOne.java
+++ Stranger/src/BankManagementSystem/SignupOne.java
@@ -5,8 +5,15 @@ import java.util.*;
 public class SignupOne  extends JFrame{
     SignupOne(){
         Random ran = new Random();
-        System.out.println(Math.abs(ran.nextLong() % 9000L) + 1000);
-        JLabel formno = new JLabel("Application FORM NO");
+        long random = (Math.abs(ran.nextLong() % 9000L) + 1000);
+
+
+
+        JLabel formno = new JLabel("APPLICATION FORM NO : " + random);
+        formno.setFont(new Font("Raleway", Font.BOLD, 38));
+        formno.setFont(new Font("Raleway", Font.BOLD, 30));
+        add(formno);
+
         getContentPane().setBackground(Color.gray);
              setSize(850, 800);
              setLocation(350,10);
