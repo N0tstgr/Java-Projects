e252220f925dc154dde927cfd825aadfa3d7fe89
Your Name <vikashkasaudhan321@gmail.com>
2025-09-24 00:57:05 +0530

    math.absolute

--- Stranger/src/BankManagementSystem/SignupOne.java
+++ Stranger/src/BankManagementSystem/SignupOne.java
@@ -1,19 +1,11 @@
 package BankManagementSystem;
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-
-
 public class SignupOne  extends JFrame{
-
-
-
-
     SignupOne(){
         Random ran = new Random();
-        System.out.println(ran);
+        System.out.println(Math.abs(ran.nextLong() % 9000L) + 1000);
         JLabel formno = new JLabel("Application FORM NO");
         getContentPane().setBackground(Color.gray);
              setSize(850, 800);
