4395a02779853427972720d41b558abcd0076e90
Your Name <vikashkasaudhan321@gmail.com>
2025-09-24 00:37:47 +0530

    sEtting up jframe

--- Stranger/src/BankManagementSystem/SignupOne.java
+++ Stranger/src/BankManagementSystem/SignupOne.java
@@ -1,10 +1,13 @@
 package BankManagementSystem;
+import javax.swing.*;
 
-public class SignupOne {
+public class SignupOne  extends JFrame{
 
 
     SignupOne(){
-
+             setSize(850, 800);
+             setLocation(350,10);
+             setVisible(true);
     }
     public static void main(String[] args) {
         new SignupOne();
