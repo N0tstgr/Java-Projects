9abc4f41bcec4981c4b2956a7ffe3a9ecb4487c9
Your Name <vikashkasaudhan321@gmail.com>
2025-10-05 16:44:46 +0530

    deletion-1

--- Stranger/src/Stgr/hi.java
+++ Stranger/src/Stgr/hi.java
@@ -3,7 +3,7 @@ package Stgr;
 public class hi {
     public static void main(String[] args) {
         System.out.println("The world is going towards the greatness");
-        System.out.println("hope you will also contribute in it to make this world beyond the earth");
+
 
     }
 }
